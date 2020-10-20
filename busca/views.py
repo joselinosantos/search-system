8e3b15783c5cf3556516b308042c5d695bbd29b5
Joselino Santos <joselinosantosti@gmail.com>
2020-10-20 09:38:11 -0300

    Add total de páginas e titulo das paginas

--- busca/views.py
+++ busca/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Urls
+import urllib3
 
 # Todas as views aqui
 def index(request):
@@ -7,6 +8,8 @@ def index(request):
 
 def paginas(request):
 	if request.method == "POST":
+		titulo = "Titulo da pagina"
 		pesquisa = request.POST.get('tf_busca')
 		paginas = Urls.objects.filter(url__contains=pesquisa).order_by('url')
-		return render(request, 'paginas.html', {'paginas': paginas})
+		total_pag = len(paginas)
+		return render(request, 'paginas.html', {'paginas': paginas, 'titulo':titulo})

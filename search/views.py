cfb84e09e706892833536ca109704088688f42cc
Joselino Santos <joselinosantosti@gmail.com>
2022-12-07 17:08:02 -0300

    Melhorias na seção de resultados

--- search/views.py
+++ search/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView
 from .models import Urls
 
@@ -7,7 +8,7 @@ class IndexView(TemplateView):
 class ResultsView(ListView):
 	model = Urls
 	template_name = 'results.html'
-	paginate_by = 10
+	# paginate_by = 10
 	context_object_name = 'results'
 	
 	def get_queryset(self):
@@ -15,9 +16,9 @@ class ResultsView(ListView):
 		try:
 			if search:
 				qs = super().get_queryset()
-				qs = qs.order_by('url').filter(url__icontains=search)
-				return qs
+				results = qs.order_by('url').filter(url__icontains=search)
+				return results
 			else:
-				print('Nenhum resultado encontrado')
+				return HttpResponse('Nenhum resultado encontrado')
 		except TypeError as e:
-			print(e)
+			return e
